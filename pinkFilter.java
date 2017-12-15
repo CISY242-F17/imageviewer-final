28fec352671c4139f152cea3159a9dfa2afaf9e4
DarrylHellams <djhellamsjr11@gmail.com>
2017-12-15 03:08:34 -0500

    Image Filter Lab
    Created orange and pink filter

---
+++ pinkFilter.java
@@ -0,0 +1,35 @@
+import java.awt.Color;
+/**
+ * An image filter to make the image a bit darker.
+ * 
+ * @author Darryl
+ * @version 2017.12.15
+ */
+public class pinkFilter extends Filter
+{
+    /**
+     * Constructor for objects of class DarkerFilter.
+     * @param name The name of the filter.
+     */
+    public pinkFilter(String name)
+    {
+        super(name);
+    }
+
+    /**
+     * Apply this filter to an image.
+     * 
+     * @param  image  The image to be changed by this filter.
+     */
+    public void apply(OFImage image)
+    {
+        int height = image.getHeight();
+        int width = image.getWidth();
+        for(int y = 0; y < height; y++) {
+            for(int x = 0; x < width; x++) {
+                Color pix = image.getPixel(x, y);
+                image.setPixel(x, y, new Color(255, 0, 180));
+            }
+        }
+    }
+}
